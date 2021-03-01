cf099fbbe7432d265ed65430ad307ed45746147f
Murray Evans <meva@ucalgary.ca>
2021-02-28 22:02:07 -0700

    added UI scoreboard

---
+++ Assets/Components/Scoreboard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    private GameObject[] snitchTag;
+    public GameObject snitch;
+    public Text score;
+    private SnitchBehaviour sb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        snitchTag = GameObject.FindGameObjectsWithTag("snitch");
+        snitch = snitchTag[0];
+        sb = snitch.gameObject.GetComponent<SnitchBehaviour>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        score.text = "Griffindor: " + sb.scoreGriffindor.ToString();
+    }
+}
